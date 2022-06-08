3f4905015c29f80c81268d68a5a525fc304152d0
Marcus ParkerGarruba <mparkergarruba@gmail.com>
2022-06-08 16:31:19 -0400

    commit tree ec0d772332116373187f6eff3aa7cb5b5a78ed77

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array) {
+    let win = array.find((item) => item.result === "W")
+    if (win) {
+        return win.year
+    }
+  }
+  
+  superbowlWin(record)
